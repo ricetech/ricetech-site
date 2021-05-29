b9c3ad3f9fe1522b6eb8fbf6ac1e29c437ef7846
Eric C <39015447+ricetech@users.noreply.github.com>
2021-05-29 18:15:59 -0400

    Add logo to navbar

--- src/components/ricetech-navbar/RiceTechNavbar.tsx
+++ src/components/ricetech-navbar/RiceTechNavbar.tsx
@@ -3,9 +3,17 @@ import { Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './_ricetech-navbar.scss';
 
+// noinspection HtmlUnknownTarget
 export const RiceTechNavbar = () => (
   <Navbar bg="dark" variant="dark" expand="md" sticky="top">
-    <Navbar.Brand as={Link} to="/">Eric Chen (RiceTech)</Navbar.Brand>
+    <Navbar.Brand as={Link} to="/" className="mr-2">
+      <img
+        src="/img/logo/the_ricetech-23.svg"
+        className="d-inline-block align-top navbar--logo"
+        alt="React Bootstrap logo"
+      />{' '}
+      Eric Chen (RiceTech)
+    </Navbar.Brand>
     <Navbar.Toggle aria-controls="ricetech-navbar" />
     <Navbar.Collapse id="ricetech-navbar">
       <Nav className="mr-auto">
