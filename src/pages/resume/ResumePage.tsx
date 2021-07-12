b48eff80e64d251d2e7d50f04ce51fabe281dd10
Eric C <39015447+ricetech@users.noreply.github.com>
2021-07-12 16:14:49 -0400

    Add resume to ResumePage

--- src/pages/resume/ResumePage.tsx
+++ src/pages/resume/ResumePage.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 import './_resume.scss';
 
 export const ResumePage = () => {
-  const x = 'ResumePage';
+  const x = 'My Resume';
   return (
-    <>
-      <h1>{x}</h1>
-    </>
+    <Container>
+      <h1 className="mt-3">{x}</h1>
+      <iframe
+        className="resume--iframe mt-3"
+        src="https://drive.google.com/file/d/1x0agAtNZFwKjTE75jqq7SzaTh2O4mocc/preview"
+        allow="autoplay"
+      />
+    </Container>
   );
 };
